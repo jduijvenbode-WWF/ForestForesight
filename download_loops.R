92468cdd18bf349e6430c80c89f0bdff07c8c596
jduijvenbode-WWF <149797028+jduijvenbode-WWF@users.noreply.github.com>
2023-11-05 19:24:49 +0100

    downloaden van forest loss

---
+++ download_loops.R
@@ -0,0 +1,18 @@
+library(httr)
+files=read.csv("D:/ff-dev/forestloss.txt",header=F)$V1
+setwd("D:/ff-dev/forestloss_2000_2022/")
+for(file in files){
+  a=gregexpr("lossyear_",file)[[1]]
+  filename=substr(file,a[1]+9,nchar(file))
+  b=httr::GET(file)
+  writeBin(b$content,filename)
+}
+library(terra)
+IA=vect("../integratedalerts.geojson")
+setwd("../alerts/")
+for(id in seq(nrow(IA))){
+  file=IA$download[id]
+  name=IA$tile_id[id]
+  b=httr::GET(file)
+  writeBin(b$content,paste0(name,".tif"))
+}
