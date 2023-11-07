aaea95a048c7b50df2ac3f2d12251e511e0e0602
jvduijvenbode <jonas@geotics.nl>
2023-11-07 11:27:34 +0100

    script for zillah

---
+++ xgboost_zillah_globalmodel.R
@@ -0,0 +1,163 @@
+alllayers=c("12-6months.tif","6months.tif","confidence.tif","deforestation_smoothed.tif",
+            "deforestation_smoothed6months.tif","totaldeforestation.tif","edgedensity_withmask.tif","edgedensity.tif",
+            "forestmask.tif","slope.tif","roughness.tif","elevation.tif","latestdeforestation.tif","groundtruth.tif")
+rasstack=rast(alllayers)
+writeRaster(rasstack,paste0("stack",ffdate,"_",extnum,".tif"),overwrite=T)
+######method 1: random sample###########
+files=list.files(pattern="stack")
+start=T
+for(file in files){
+  rasstack=rast(file)
+  dts=spatSample(rasstack,size=ncol(rasstack)*nrow(rasstack),xy=T,method="regular")
+  names(dts)=c("x","y",gsub(".tif","",alllayers))
+  if(start){fulldts=dts}else{fulldts=rbind(fulldts,dts)}
+}
+
+dts=fulldts
+write.table(dts,"helpers/powerbicsv.csv",dec=",",sep=";",row.names=F)
+dts[is.na(dts)]=0
+label_s=dts$groundtruth
+dts$groundtruth=NULL
+#sample test data and exclude test data from training data
+testsamples=sample(seq(nrow(dts)),round(nrow(dts)/4))
+testdts=dts[testsamples,]
+testdts=as.matrix(testdts)
+dts=dts[-testsamples,]
+test_label_s=label_s[testsamples]
+label_s=label_s[-testsamples]
+dts=as.matrix(dts)
+deforestation_count=sum(rowSums(dts[,3:8])>0)
+nonforestindices=which(rowSums(dts[,3:8])==0)
+remove_indices=sample(nonforestindices,length(nonforestindices)-deforestation_count)
+dts=dts[-remove_indices,]
+label_s=label_s[-remove_indices]
+label_s=as.matrix(label_s)
+label_s[label_s>1]=1
+#boost and predict
+eta=0.1
+subsample=0.9
+nrounds=500
+depth=9
+
+bst <- xgboost(data = dts, label = label_s,
+               max_depth = depth, eta = eta,subsample=subsample,  nrounds = nrounds,early_stopping_rounds = 3,
+               objective = "binary:logistic",eval_metric="aucpr",verbose = T)
+
+pred <- predict(bst, testdts)
+
+startF05=0
+for(i in quantile(pred,seq(0.9,1,0.001))){
+  a=table((pred > i)*2+(test_label_s>0))
+  UA=round(a[4]/(a[3]+a[4]),2)
+  PA=round(a[4]/(a[2]+a[4]),2)
+  F05=round(1.25*UA*PA/(0.25*UA+PA),2)
+  if(!is.na(F05)){
+    if(F05>startF05){
+      threshold=i
+      startF05=F05
+      sUA=UA
+      sPA=PA
+    }}
+
+}
+cat(paste("threshold:",threshold,"eta:",eta,"subsample:",subsample,"nrounds:",nrounds,"depth:",depth,"UA:",100*sUA,", PA:",100*sPA,"F05:",startF05,"\n"))
+
+
+
+######method 2: other date###########
+files=list.files(pattern="stack")
+files=files[-grep("2022",files)]
+start=T
+for(file in files){
+  rasstack=rast(file)
+  dts=spatSample(rasstack,size=ncol(rasstack)*nrow(rasstack),xy=T,method="regular")
+  names(dts)=c("x","y",gsub(".tif","",alllayers))
+  if(start){fulldts=dts}else{fulldts=rbind(fulldts,dts)}
+}
+
+dts=fulldts
+#write.table(dts,"helperspowerbicsv.csv",dec=",",sep=";",row.names=F)
+dts[is.na(dts)]=0
+label_s=dts$groundtruth
+dts$groundtruth=NULL
+dts$latestdeforestation=NULL
+#sample test data and exclude test data from training data
+dts=as.matrix(dts)
+label_s=as.matrix(label_s)
+label_s[label_s>1]=1
+#boost and predict
+eta=0.1
+subsample=0.9
+nrounds=1000
+depth=9
+
+bst <- xgboost(data = dts, label = label_s,
+               max_depth = depth, eta = eta,subsample=subsample,  nrounds = nrounds,early_stopping_rounds = 3,
+               objective = "binary:logistic",eval_metric="aucpr",verbose = F)
+
+pred_train <- predict(bst, dts)
+falsepositives=which((2*(pred_train>0.5)-label_s)==1)
+deforestation_count=sum(rowSums(dts[,3:8])>0)
+nonforestindices=which(rowSums(dts[,3:8])==0)
+remove_indices=sample(nonforestindices,length(nonforestindices)-deforestation_count+length(falsepositives))
+dts=dts[-remove_indices,]
+label_s=label_s[-remove_indices]
+
+#####test part#####
+files=list.files(pattern="stack")
+files=files[grep("2022",files)]
+start=T
+for(file in files){
+  rasstack=rast(file)
+  dts=spatSample(rasstack,size=ncol(rasstack)*nrow(rasstack),xy=T,method="regular")
+  names(dts)=c("x","y",gsub(".tif","",alllayers))
+  if(start){fulldts=dts}else{fulldts=rbind(fulldts,dts)}
+}
+
+testdts=fulldts
+#write.table(dts,"helperspowerbicsv.csv",dec=",",sep=";",row.names=F)
+testdts[is.na(testdts)]=0
+test_label_s=testdts$groundtruth
+testdts$groundtruth=NULL
+testdts$latestdeforestation=NULL
+#sample test data and exclude test data from training data
+testdts=as.matrix(testdts)
+test_label_s=as.matrix(test_label_s)
+test_label_s[test_label_s>1]=1
+
+pred <- predict(bst, testdts)
+
+startF05=0
+for(i in quantile(pred,seq(0.9,1,0.001))){
+  a=table((pred > i)*2+(test_label_s>0))
+  UA=round(a[4]/(a[3]+a[4]),2)
+  PA=round(a[4]/(a[2]+a[4]),2)
+  F05=round(1.25*UA*PA/(0.25*UA+PA),2)
+  if(!is.na(F05)){
+    if(F05>startF05){
+      threshold=i
+      startF05=F05
+      sUA=UA
+      sPA=PA
+    }}
+
+}
+cat(paste("threshold:",threshold,"eta:",eta,"subsample:",subsample,"nrounds:",nrounds,"depth:",depth,"UA:",100*sUA,", PA:",100*sPA,"F05:",startF05,"\n"))
+
+
+############train set############
+
+
+
+
+pred_train <- predict(bst, dts)
+
+
+
+for(i in quantile(pred,seq(0.9,1,0.001))){
+  a=table((pred_train > i)*2+(label_s>0))
+  UA=round(a[4]/(a[3]+a[4]),2)
+  PA=round(a[4]/(a[2]+a[4]),2)
+  F05=round(1.25*UA*PA/(0.25*UA+PA),2)
+  cat(paste("quantile:",round(i,2), "UA:",100*UA,", PA:",100*PA,"F05:",F05,"\n"))
+}
