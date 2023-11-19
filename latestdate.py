adc373b17c011043b52878823f659d0b8354c45c
jvduijvenbode <jonas@geotics.nl>
2023-11-19 22:42:03 +0100

    fixed so no unnecessary stuff is run

--- latestdate.py
+++ latestdate.py
@@ -31,7 +31,20 @@ def process_geotiff(input_file, output_file,relative_date):
         num_windows = 4
         window_width = width // 2
         window_height = height // 2
-
+        latest_deforestation_file=output_file.replace("layer","latestdeforestation")
+        create_latest_deforestation= not os.path.isfile(latest_deforestation_file)
+        threemonths_file=output_file.replace("layer","3months")
+        create_threemonths = not os.path.isfile(threemonths_file)
+        sixmonths_file=output_file.replace("layer","6months")
+        create_sixmonths = not os.path.isfile(sixmonths_file) 
+        twelvetosixmonths_file=output_file.replace("layer","12-6months")
+        create_twelvetosixmonths = not os.path.isfile(twelvetosixmonths_file)
+        totaldeforestation_file=output_file.replace("layer","totaldeforestation")
+        create_totaldeforestation = not os.path.isfile(totaldeforestation_file)
+        groundtruth_file=output_file.replace("layer","groundtruth")
+        create_groundtruth = not os.path.isfile(groundtruth_file)
+        confidence_file=output_file.replace("layer","confidence")
+        create_confidence = not os.path.isfile(confidence_file)
         # Iterate over windows
         for i in range(num_windows):
             # Calculate the starting coordinates of the window
@@ -52,58 +65,58 @@ def process_geotiff(input_file, output_file,relative_date):
                 twelvetosixmonths=template.copy()
                 totaldeforestation=template.copy()
                 groundtruth=template.copy()
-                #confidence=template.copy()
+                confidence=template.copy()
             offx2=(offx1+(template.shape[0]//2))
             offy2=(offy1+(template.shape[1]//2))
 
 
             # Take the remainder of 10000 for every pixel
-            confidence[offx1:offx2,offy1:offy2]=aggregate_by_40_max((np.remainder(np.nan_to_num(data), 10000)<relative_date).astype(int)*data//10000,fun="nanmean")
+            if create_confidence: confidence[offx1:offx2,offy1:offy2]=aggregate_by_40_max((np.remainder(np.nan_to_num(data), 10000)<relative_date).astype(int)*data//10000,fun="nanmean")
             data = np.remainder(np.nan_to_num(data), 10000)
 
 
             #remove current date from data to get relative date, ignoring 0's, then remove everything below 0 to remove future deforestation. then aggregate by 40.     
-            latest_deforestation[offx1:offx2,offy1:offy2]=aggregate_by_40_max(np.multiply(np.maximum(np.divide(data,relative_date, where=data>0),1),10000).astype(int),fun="max")
+            if create_latest_deforestation: latest_deforestation[offx1:offx2,offy1:offy2]=aggregate_by_40_max(np.multiply(np.maximum(np.divide(data,relative_date, where=data>0),1),10000).astype(int),fun="max")
 
             #remove current date from data to get relative date, ignoring 0's, then remove everything below 0 to remove future deforestation. then aggregate by 40.  
-            threemonths[offx1:offx2,offy1:offy2]=aggregate_by_40_max(((data<=relative_date)&(data>(relative_date-92))).astype(int),fun="sum")
-            sixmonths[offx1:offx2,offy1:offy2]=aggregate_by_40_max(((data<=relative_date)&(data>(relative_date-183))).astype(int),fun="sum")
-            twelvetosixmonths[offx1:offx2,offy1:offy2]=aggregate_by_40_max(((data<=(relative_date-183))&(data>(relative_date-366))).astype(int),fun="sum")
-            totaldeforestation[offx1:offx2,offy1:offy2]=aggregate_by_40_max((data<=(relative_date)).astype(int),fun="sum")
-            groundtruth[offx1:offx2,offy1:offy2]=aggregate_by_40_max(((data<=(relative_date+182))&(data>relative_date)).astype(int),fun="max")
+            if create_threemonths: threemonths[offx1:offx2,offy1:offy2]=aggregate_by_40_max(((data<=relative_date)&(data>(relative_date-92))).astype(int),fun="sum")
+            if create_sixmonths: sixmonths[offx1:offx2,offy1:offy2]=aggregate_by_40_max(((data<=relative_date)&(data>(relative_date-183))).astype(int),fun="sum")
+            if create_twelvetosixmonths: twelvetosixmonths[offx1:offx2,offy1:offy2]=aggregate_by_40_max(((data<=(relative_date-183))&(data>(relative_date-366))).astype(int),fun="sum")
+            if create_totaldeforestation: totaldeforestation[offx1:offx2,offy1:offy2]=aggregate_by_40_max((data<=(relative_date)).astype(int),fun="sum")
+            if create_groundtruth: groundtruth[offx1:offx2,offy1:offy2]=aggregate_by_40_max(((data<=(relative_date+182))&(data>relative_date)).astype(int),fun="max")
 
-        latest_deforestation_file=output_file.replace("layer","latestdeforestation")        
-        if not os.path.isfile(latest_deforestation_file):
+               
+        if create_latest_deforestation:
             with rasterio.open(latest_deforestation_file, 'w', driver='GTiff',compress='LZW', width=width//40, height=height//40, count=1, dtype=src.dtypes[0], crs=src.crs, transform=newtransform) as dst:
                 dst.write(latest_deforestation.reshape(1,latest_deforestation.shape[0],latest_deforestation.shape[1]))
 
-        threemonths_file=output_file.replace("layer","3months")
-        if not os.path.isfile(threemonths_file):
+
+        if create_threemonths:
             with rasterio.open(threemonths_file, 'w', driver='GTiff',compress='LZW', width=width//40, height=height//40, count=1, dtype=src.dtypes[0], crs=src.crs, transform=newtransform) as dst:
                 dst.write(threemonths.reshape(1,threemonths.shape[0],threemonths.shape[1]))
 
-        sixmonths_file=output_file.replace("layer","6months")
-        if not os.path.isfile(sixmonths_file):
+        
+        if create_sixmonths:
             with rasterio.open(threemonths_file, 'w', driver='GTiff',compress='LZW', width=width//40, height=height//40, count=1, dtype=src.dtypes[0], crs=src.crs, transform=newtransform) as dst:
                 dst.write(sixmonths.reshape(1,sixmonths.shape[0],sixmonths.shape[1]))
 
-        twelvetosixmonths_file=output_file.replace("layer","12-6months")
-        if not os.path.isfile(twelvetosixmonths_file):
+        
+        if create_twelvetosixmonths:
             with rasterio.open(twelvetosixmonths_file, 'w', driver='GTiff',compress='LZW', width=width//40, height=height//40, count=1, dtype=src.dtypes[0], crs=src.crs, transform=newtransform) as dst:
                 dst.write(twelvetosixmonths.reshape(1,twelvetosixmonths.shape[0],twelvetosixmonths.shape[1]))
 
-        totaldeforestation_file=output_file.replace("layer","totaldeforestation")
-        if not os.path.isfile(totaldeforestation_file):
+        
+        if create_totaldeforestation:
             with rasterio.open(totaldeforestation_file, 'w', driver='GTiff',compress='LZW', width=width//40, height=height//40, count=1, dtype=src.dtypes[0], crs=src.crs, transform=newtransform) as dst:
                 dst.write(totaldeforestation.reshape(1,totaldeforestation.shape[0],totaldeforestation.shape[1]))
 
-        groundtruth_file=output_file.replace("layer","groundtruth")
-        if not os.path.isfile(groundtruth_file):
+        
+        if create_groundtruth:
             with rasterio.open(groundtruth_file, 'w', driver='GTiff',compress='LZW', width=width//40, height=height//40, count=1, dtype=src.dtypes[0], crs=src.crs, transform=newtransform) as dst:
                 dst.write(groundtruth.reshape(1,groundtruth.shape[0],groundtruth.shape[1]))
 
-        confidence_file=output_file.replace("layer","confidence")
-        if not os.path.isfile(confidence_file):
+        
+        if create_confidence:
             with rasterio.open(confidence_file, 'w', driver='GTiff',compress='LZW', width=width//40, height=height//40, count=1, dtype=src.dtypes[0], crs=src.crs, transform=newtransform) as dst:
                 dst.write(confidence.reshape(1,confidence.shape[0],confidence.shape[1]))
 
