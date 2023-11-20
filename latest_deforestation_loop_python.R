79c4a38551a5a786f298799588dad30071b4d34d
jduijvenbode-WWF <149797028+jduijvenbode-WWF@users.noreply.github.com>
2023-11-20 16:15:01 +0100

    created new script for deforestation which also does all other non-focal layers. also made some changes to the maintain script

--- latest_deforestation_loop_python.R
+++ latest_deforestation_loop_python.R
@@ -1,26 +1,19 @@
-latestdeforestation=function(ras,diffdate,filename){
-  if(!file.exists(filename)){
-    aggregate(diffdate-(ras%%10000),40,fun="max",na.rm=T,filename=filename,overwrite=T)
-  }
-}
+
+source("C:/Users/EagleView/Documents/GitHub/ForestForesight/functions.R")
 files=list.files("D:/ff-dev/alerts/",full.names=T)
 firstindices=c("D:/ff-dev/alerts/20N_080W.tif", "D:/ff-dev/alerts/10N_080W.tif", "D:/ff-dev/alerts/00N_080W.tif", "D:/ff-dev/alerts/10N_070W.tif"
                ,"D:/ff-dev/alerts/00N_070W.tif", "D:/ff-dev/alerts/10S_080W.tif")
 files=files[c(which(files %in% firstindices),which(!files %in% firstindices))]
 firstindices=c("D:/ff-dev/alerts/10N_080W.tif")
 files=files[c(which(files %in% firstindices),which(!files %in% firstindices))]
-
-ffdates=paste(sort(rep(c(2021,2022,2023),12)),seq(12),"01",sep="-")
+ffdates=paste(sort(rep(c(2021,2022,2023),12)),seq(12),"01",sep="-")[1:29]
+setwd("D:/ff-dev/results/")
 for(file in files){
-  ras=rast(file)
+  print(file)
   for(ffdate in ffdates){
     print(ffdate)
+    newfile=file.path(getwd(),createfilename(createrasname(file),date = ffdate,layer="layer"))
     diffdate=as.numeric(as.Date(ffdate)-as.Date("2015-01-01"))
-    rasname=createrasname(file)
-    newfilename=createfilename(rasname,date=ffdate,layer="latestdeforestation",number=NA)
-    if(!file.exists(newfilename)){
-      latestdeforestation(ras,diffdate,newfilename)
-    }
+    system(paste("python","C:/Users/EagleView/Documents/GitHub/ForestForesight/latestdate.py",file,newfile,diffdate),intern=T)
   }
 }
-  
--- maintain_overview_2.R
+++ maintain_overview_2.R
@@ -5,10 +5,7 @@ intal_bucket="wwf-ff-global"
 Sys.setenv("AWS_DEFAULT_REGION"="eu-west-1")
 areas=vect("../integratedalerts.geojson")
 world=vect("../borders.geojson")
-completefile=list.files(recursive=T,pattern="tif$")
-overview=t(t(table(dirname(completefile))))
-overview=data.frame("tile_id"=row.names(overview),count=overview[,1])
-areas=merge(areas,overview,by="tile_id")
+
 
 
 # Set the bucket and object details
@@ -17,11 +14,15 @@ areas=merge(areas,overview,by="tile_id")
 
 
 while(T){
+  completefile=list.files(recursive=T,pattern="tif$")
+  overview=t(t(table(dirname(completefile))))
+  overview=data.frame("tile_id"=row.names(overview),count=overview[,1])
+  areas2=merge(areas,overview,by="tile_id")
   ###########overview visualsation#########
   png("overview.png",width=2000,height=1100)
   plot(world)
-  plot(areas,"count",add=T,alpha=0.5,
-       breaks=floor(unique(quantile(overview$count,seq(0,1,0.1)))),
+  plot(areas2,"count",add=T,alpha=0.5,
+       breaks=unique(floor(quantile(overview$count,seq(0,1,0.1)))),
        breakby="cases",legend="bottomleft",
        plg=list( # parameters for drawing legend
          title = paste(substr(Sys.time(),1,16),"\nprogress:",sum(overview$count),paste0("(",round(sum(overview$count)/(1.03*max(overview$count))),"%)")),
@@ -33,7 +34,7 @@ while(T){
        ), 
        cex.main = 3 # Title text size
   )
-  text(areas,paste0(areas$tile_id,"\n",areas$count),halo=T,cex=0.7)
+  text(areas2,paste0(areas$tile_id,"\n",areas2$count),halo=T,cex=0.7)
   
   dev.off()
   a=s3sync(bucket=intal_bucket,verbose=F)
