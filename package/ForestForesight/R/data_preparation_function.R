533a8154b9328d10ef0eec706f98626f417ea022
jvduijvenbode <jonas@geotics.nl>
2023-12-18 17:25:56 +0100

    created the first final version of the forestforesight package

--- package/ForestForesight/R/data_preparation_function.R
+++ package/ForestForesight/R/data_preparation_function.R
@@ -1,24 +1,67 @@
-data_preparation=function(datafolder=NA,country=NA,countryfile=NA,tilesfile=NULL,tiles=NULL,groundtruth_pattern="groundtruth",training_start=c(2021,1),training_end=c(2022,12),test_month,inc_features=NULL,exc_features=NULL,fltr_features="forestmask2019",fltr_condition=">0",random_sample_size=1,apply_filter_on_test=T,relativedate=T){
-  #folder should be a path to the folder that contains the 10-degree folders. Alternatively make sure your environment variable xgboost_datafolder is that folder
-  #country should be a 3-letter iso code (Gabon=GAB,Laos=LAO). If this parameter is set, countryfile and tilefile should also be set
-  #countryfile is only used when country is set and should contain a column called iso3.
+#' Prepare data for machine learning.
+#'
+#' This function prepares data for machine learning tasks based on specified parameters.
+#'
+#' @param datafolder Path to the data folder. Default is the system variable xgboost_datafolder. should contain the degrees folders
+#' @param country Country for which the data is prepared. Is optional when tiles are given. Should be the ISO3 code.
+#' @param countryfile Path to the country file. Not necessary if the file borders.geojson is in the datafolder.
+#' @param tilesfile Path to the tiles file. Not necessary if the file integratedalerts.geojson is in the datafolder.
+#' @param tiles Vector of tiles in the syntax of e.g. 10N_080W.
+#' @param groundtruth_pattern Pattern to identify ground truth files. 'groundtruth'.
+#' @param training_start Start date for training data in the format c(YYYY, M). Default is c(2021, 1).
+#' @param training_end End date for training data in the format c(YYYY, M). Default is c(2022, 12).
+#' @param test_month Test month for evaluation in the format c(YYYY, M). Example: c(2023,5).
+#' @param inc_features Vector of included features. States which features to include in the data preparation.
+#' @param exc_features Vector of excluded features. States which features to exclude in the data preparation.
+#' @param fltr_features vector of features for filtering data. Default is 'forestmask2019'. needs to be combined with fltr_condition of the same length
+#' @param fltr_condition Vector of filtering conditions. Default is '>0'. Should consist of operator and value and needs to be combined with fltr_features of same length vector
+#' @param validation_sample float between 0 and 1 that indicates how much of the training dataset should be used for validation. Default is 0. Advised is to not set it above 0.3
+#' @param random_sample_size Fraction size of the random sample. Should be bigger than 0 and smaller or equal to 1. Default is 1
+#' @param apply_filter_on_test Boolean indicating whether to apply the filters in fltr_features also on test data. Default is TRUE.
+#' @param relativedate Boolean indicating whether the date is relative. Default is \code{TRUE}.
+#'
+#' @return A prepared dataset for machine learning.
+#' @export
+#'
+#' @seealso
+#' \code{\link{other_function}}
+#' \code{\link{another_function}}
+#'
+#' @references
+#' Jonas van Duijvenbode (2023)
+#'
+#' @keywords XGBoost data preparation
+#' @rdname data_preparation
+#' @name data_preparation
+
+
+data_preparation=function(datafolder=NA,country=NA,countryfile=NA,tilesfile=NULL,tiles=NULL,groundtruth_pattern="groundtruth",training_start=c(2021,1),training_end=c(2021,3),test_month,inc_features=NA,exc_features=NA,fltr_features="forestmask2019",fltr_condition=">0",validation_sample=0,random_sample_size=1,apply_filter_on_test=T,relativedate=T){
   library(terra);library(sf);library(lubridate)
   if(is.na(datafolder)){datafolder=Sys.getenv("xgboost_datafolder")}
-  curwd=getwd();setwd(datafolder)
+  if(datafolder==""){stop("No environment variable for xgboost_datafolder and no datafolder parameter set")}
+  data(gfw_tiles)
+  tilesvect=vect(gfw_tiles)
   if(!is.na(country)){
-    if(!is.na(countryfile)){borders=vect(sf::st_read(countryfile))}else{if(file.exists("borders.geojson")){borders=vect(sf::st_read("borders.geojson"))}else{break("no country file submitted and no borders.geojson in datafolder")}}
-    if(!is.null(tilesfile)){tilesvect=vect(tilesfile)}else{if(file.exists("integratedalerts.geojson")){tilesvect=vect("integratedalerts.geojson")}else{break("no country file submitted and no integratedalerts.geojson in datafolder")}}
-    covertiles=tilesvect[borders[which(borders$iso3==country)]]$tile_id
-    if(is.null(tiles)){tiles=covertiles}
-  }
+    data(countries)
+    borders=vect(countries)
+    tilesvect=tilesvect[borders[which(borders$iso3==country)]]$tile_id
+    if(is.null(tiles)){tiles=tilesvect}
+    }
+
   #list all the files for the tiles that have been selected
-  allfiles=as.character(unlist(sapply(tiles,function(x) list.files(full.names=T,path=x,recursive=T,pattern="tif$"))))
+  allfiles=as.character(unlist(sapply(tiles,function(x) list.files(path=file.path(datafolder,x),full.names=T,recursive=T,pattern="tif$"))))
+  if(length(allfiles)==0){stop(paste("no folders with tif-files found that correspond to the given tile id's:",paste(tiles,collapse=",")))}
   #remove features that are not wanted
   #TODO sm6months would be removed if we would filter on 6months for instance
-  if(!is.null(exc_features)){allfiles=allfiles[-unique(unlist(sapply(exc_features,function(x) grep(x,allfiles))))]}
-  if(!is.null(inc_features)){allfiles=allfiles[unique(unlist(sapply(inc_features,function(x) grep(x,allfiles))))]}
+  if(!is.na(exc_features)){
+    exc_indices=unique(unlist(sapply(exc_features,function(x) grep(x,allfiles))))
+    if(length(exc_indices)>0){allfiles=allfiles[-exc_indices]}}
+  if(!is.na(inc_features)){
+    inc_indices=unique(unlist(sapply(inc_features,function(x) grep(x,allfiles))))
+    if(length(inc_indices>0)){allfiles=allfiles[inc_indices]}}
   #create the range between start and end date
   training_daterange=as.character(seq(as.Date(paste0(training_start[1],"-",sprintf("%02d",training_start[2]),"-01")),as.Date(paste0(training_end[1],"-",sprintf("%02d",training_end[2]),"-01")),"1 month"))
+
   for(tile in tiles){
     files=allfiles[grep(tile,allfiles)]
     static_files= files[-grep("01\\.",files)]
@@ -29,11 +72,9 @@ data_preparation=function(datafolder=NA,country=NA,countryfile=NA,tilesfile=NULL
     test_month=as.character(as.Date(paste0(test_month[1],"-",sprintf("%02d",test_month[2]),"-01")))
     for(i in c(training_daterange,test_month)){
       dynamic_files = sort(files[grep(i,files)])
-      #print(dynamic_files);print(static_files)
       rasstack=c(rast(dynamic_files,win=ext(rast(static_files[1]))),rast(static_files,win=ext(rast(static_files[1]))))
       dts=as.matrix(rasstack)
       coords=xyFromCell(rasstack,seq(ncol(rasstack)*nrow(rasstack)))
-      filedate=substr(file,nchar(file)-13,nchar(file)-4)
       dts=cbind(coords,dts)
       if(relativedate){dts=cbind(dts,rep(abs(round(as.numeric(as.Date(i))%%365.25)-183),nrow(dts)))}
       dts[is.na(dts)]=0
@@ -60,9 +101,7 @@ data_preparation=function(datafolder=NA,country=NA,countryfile=NA,tilesfile=NULL
   }
   if(!exists("testfilterindices")){testfilterindices=NA}
   #split data into feature data and label data
-  print(colnames(traindts))
   groundtruth_index=which(colnames(traindts)==groundtruth_pattern)
-  print(groundtruth_index)
   train_label=traindts[,groundtruth_index]
   traindts=traindts[,-groundtruth_index]
   groundtruth_index=which(colnames(testdts)==groundtruth_pattern)
@@ -72,14 +111,15 @@ data_preparation=function(datafolder=NA,country=NA,countryfile=NA,tilesfile=NULL
   #make sure that label data is binary
   train_label[train_label>1]=1
   test_label[test_label>1]=1
-  print("nondeju")
-  #create a watchlist that can be used by xgboost
-  print(nrow(traindts));print(length(train_label))
-  print(nrow(testdts));print(length(test_label))
-  watchlist = list(train = xgb.DMatrix(traindts, label=train_label), eval = xgb.DMatrix(testdts, label=test_label))
-  #set the working directory back to original
-  setwd(curwd)
-  print("godver")
-  return(list("watchlist"=watchlist,"testindices"=testfilterindices))
+  if(validation_sample>0){
+    sample_indices=sample(seq(nrow(traindts)),round(validation_sample*nrow(traindts)))
+    train_matrix=xgb.DMatrix(traindts[sample_indices,], label=train_label[sample_indices])
+    validation_matrix=xgb.DMatrix(traindts[-sample_indices,], label=train_label[-sample_indices])
+  }else{
+    train_matrix=xgb.DMatrix(traindts, label=train_label)
+    validation_matrix=NA
+  }
+  return(list("train_matrix"=train_matrix,"validation_matrix"=validation_matrix,
+              "test_matrix"= xgb.DMatrix(testdts, label=test_label),"testindices"=testfilterindices,"groundtruth"=test_label))
 }
-test=data_preparation(tiles="10N_080W",training_start=c(2022,01),training_end = c(2022,1),test_month = c(2022,9),random_sample_size = 0.6,exc_features = c("sm6months","loss2022"))
+
---
+++ package/ForestForesight/R/data_quality_check.R
@@ -0,0 +1,50 @@
+#' Data quality control
+#'
+#' This function loops through all tif files in a given folder and gives a summary and total overview of the quality of the rasters
+#'
+#' @param folder_path The path to the folder containing TIF files.
+#'
+#' @examples
+#' # Example usage:
+#' result <- process_tif_files("path/to/tif_folder", test_integrity = TRUE)
+#'
+#' @export
+#'
+#'
+data_quality <- function(folder_path) {
+
+  summary_by_feature=function(dataframe,feature){
+    seldf=dataframe[which(dataframe$featurenames==feature),]
+    return(list("feature"=feature,
+                "mindate"=min(as.Date(seldf$dates)),
+                "maxdate"=max(as.Date(seldf$dates)),
+                "gaps"=if(max(diff(sort(as.Date(seldf$dates))))<32){"no"}else{"yes"},
+                "doubles"=if(min(diff(sort(as.Date(seldf$dates))))>27){"no"}else{"yes"},
+                "npixel"=if(sd(seldf$npixel)==0){"equal"}else{"diff"},
+                "xmin"=if(sd(seldf$xmin)==0){"equal"}else{"diff"},
+                "xmax"=if(sd(seldf$xmax)==0){"equal"}else{"diff"},
+                "ymin"=if(sd(seldf$ymin)==0){"equal"}else{"diff"},
+                "ymax"=if(sd(seldf$ymax)==0){"equal"}else{"diff"},
+                "resolution"=if(sd(seldf$resolution)==0){"equal"}else{"diff"},
+                "crsname"=if(length(unique(seldf$crsname))==1){"equal"}else{seldf$crsname[1]},
+                "crscode"=if(length(unique(seldf$crscode))==1){"equal"}else{seldf$crscode[1]},
+                "mean"=mean(seldf$mean),
+                "max"=max(seldf$max),
+                "hasNA"=if(length(unique(seldf$hasNA))==1){"equal"}else{"diff"}))
+  }
+  # Get a list of all TIF files in the folder
+  tif_files <- list.files(folder_path, pattern = "\\.tif$", full.names = TRUE)
+  valuelist=lapply(tif_files,function(x) summarize_rast(x))
+  allvals=as.data.frame(as.matrix(do.call(rbind,valuelist)))
+  names(allvals)=names(valuelist[[1]])
+  for(i in 1:ncol(allvals)){allvals[,i]=unlist(allvals[,i])}
+  allvals$featurenames=as.character(unlist(sapply(tif_files,function(x) strsplit(gsub(pattern = "\\.tif",replacement="",basename(x)),"_")[[1]][1])))
+  allvals$dates=as.character(unlist(sapply(tif_files,function(x) strsplit(gsub(pattern = "\\.tif",replacement="",basename(x)),"_")[[1]][2])))
+  allvals=allvals[,c(12,13,seq(11))]
+  # Initialize data frame to store quality flags
+  summary=lapply(unique(allvals$featurenames),function(x) summary_by_feature(allvals,x))
+  summarytable=as.data.frame(do.call(rbind,summary))
+  names(summarytable)=names(summary[[1]])
+  for(i in 1:ncol(summarytable)){summarytable[,i]=unlist(summarytable[,i])}
+  return(list("summary"=summarytable,"all"=allvals))
+}
