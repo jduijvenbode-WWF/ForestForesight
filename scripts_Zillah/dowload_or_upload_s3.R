888b48b3ba2b51297d12a1695b82d635f787801e
kingkong-WWF <jduijvenbode@wwf.nl>
2023-12-19 14:27:57 +0100

    Script Laos added

--- scripts_Zillah/dowload_or_upload_s3.R
+++ scripts_Zillah/dowload_or_upload_s3.R
@@ -7,14 +7,14 @@ Sys.setenv("AWS_DEFAULT_REGION"="eu-west-1")
 features= c("elevation.tif", "loss2001-2018.tif","loss2019.tif","loss2020.tif","loss2021.tif","loss2022.tif","slope.tif")
 
 for (tile in tiles){
-  for (feature in features[1]){
+  for (feature in features){
     object_key <- paste0(tile, '/', feature)
     del_key= paste0(tile, '/lastloss.tif')
     local_path <- file.path(inputdir, tile, feature)
     print(paste("Put", local_path, "to", object_key))
-    delete_object(object=del_key, bucket="wwf-ff-global")
+    #delete_object(object=del_key, bucket="wwf-ff-global")
     #put_object(file=local_path, object=object_key, bucket="wwf-ff-global")
-    #save_object(object = object_key, bucket = "wwf-ff-global", file = local_path, quiet = FALSE, overwrite=TRUE)
+    save_object(object = object_key, bucket = "wwf-ff-global", file = local_path, quiet = FALSE, overwrite=TRUE)
   }
 }
 
