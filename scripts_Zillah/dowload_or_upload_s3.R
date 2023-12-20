bb84b8ce3284e40eb527961681900685194914fc
kingkong-WWF <jduijvenbode@wwf.nl>
2023-12-20 16:25:48 +0100

    monthly tests
    Test on 12 months

--- scripts_Zillah/dowload_or_upload_s3.R
+++ scripts_Zillah/dowload_or_upload_s3.R
@@ -4,23 +4,27 @@ tiles=list.files(file.path(inputdir), pattern ="_")
 
 Sys.setenv("AWS_DEFAULT_REGION"="eu-west-1")
 
-features= c("elevation.tif", "loss2001-2018.tif","loss2019.tif","loss2020.tif","loss2021.tif","loss2022.tif","slope.tif")
+features= c("pop2020.tif","pop202.tif","pop2030.tif")
 
 for (tile in tiles){
   for (feature in features){
     object_key <- paste0(tile, '/', feature)
-    del_key= paste0(tile, '/lastloss.tif')
+    del_key= paste0(tile, '/', feature)
     local_path <- file.path(inputdir, tile, feature)
-    print(paste("Put", local_path, "to", object_key))
-    #delete_object(object=del_key, bucket="wwf-ff-global")
+    print(paste("delete:", del_key))
+    delete_object(object=del_key, bucket="wwf-ff-global")
     #put_object(file=local_path, object=object_key, bucket="wwf-ff-global")
-    save_object(object = object_key, bucket = "wwf-ff-global", file = local_path, quiet = FALSE, overwrite=TRUE)
+    #save_object(object = object_key, bucket = "wwf-ff-global", file = local_path, quiet = FALSE, overwrite=TRUE)
   }
 }
 
 for (tile in tiles){
-  local_path <- file.path(inputdir, tile, "lastloss.tif")
-  file.remove(local_path)
+  for (file in c("pop2020.tif","pop202.tif","pop2030.tif")){
+    local_path <- file.path(inputdir, tile, file)
+    file.remove(local_path)
+    print(paste("removed :", local_path))
+  }
+  
 }
 
 
