d4db8aa62b05c8e2dc6e82255848cc1f5dfec075
jvduijvenbode <jonas@geotics.nl>
2023-12-20 08:42:22 +0100

    updated version that outputs the raster for groundtruth analysis and merges based on identical columns

--- package/ForestForesight/R/ff_dqc.R
+++ package/ForestForesight/R/ff_dqc.R
@@ -12,7 +12,6 @@
 #'
 #'
 ff_dqc <- function(folder_path) {
-
   summary_by_feature=function(dataframe,feature){
     type="dynamic"
     seldf=dataframe[which(dataframe$featurenames==feature),]
@@ -43,6 +42,7 @@ ff_dqc <- function(folder_path) {
   for(i in 1:ncol(allvals)){allvals[,i]=unlist(allvals[,i])}
   allvals$featurenames=as.character(unlist(sapply(tif_files,function(x) strsplit(gsub(pattern = "\\.tif",replacement="",basename(x)),"_")[[1]][1])))
   allvals$dates=as.character(unlist(sapply(tif_files,function(x) strsplit(gsub(pattern = "\\.tif",replacement="",basename(x)),"_")[[1]][2])))
+  incorrect_dateformats=sum(!(nchar(allvals$dates)==10))
   allvals=allvals[,c(ncol(allvals)-1,ncol(allvals),seq(ncol(allvals)-2))]
   # Initialize data frame to store quality flags
   summary=suppressWarnings(lapply(unique(allvals$featurenames),function(x) summary_by_feature(allvals,x)))
@@ -50,6 +50,8 @@ ff_dqc <- function(folder_path) {
   names(summarytable)=names(summary[[1]])
   for(i in 1:ncol(summarytable)){summarytable[,i]=unlist(summarytable[,i])}
   summarytable=summarytable[order(summarytable$type),]
+  dyn_summary=summarytable[which(!is.na(summarytable$mindate)),]
+  datecheck=all(dyn_summary$gaps=="no")&all(dyn_summary$doubles=="no")&(length(unique(dyn_summary$mindate))==1)&(length(unique(dyn_summary$maxdate))==1)
   extentcheck=length(c(unique(allvals$npixel),unique(allvals$xmin),unique(allvals$xmax),unique(allvals$ymin),unique(allvals$ymax),unique(allvals$resolution)))==6
-  return(list("tile"=basename(folder_path),"byfeature"=summarytable,"all"=allvals,"equalextent"=extentcheck))
+  return(list("tile"=basename(folder_path),"byfeature"=summarytable,"all"=allvals,"equalextent"=extentcheck,"equaldaterange"=datecheck,"incorrect_dateformats"=incorrect_dateformats))
 }
