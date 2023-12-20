c38bb686851a31372042a30ae03fe7febf49dd60
kingkong-WWF <jduijvenbode@wwf.nl>
2023-12-20 08:58:16 +0100

    Update Laos.R

--- scripts_Zillah/Laos.R
+++ scripts_Zillah/Laos.R
@@ -5,7 +5,6 @@
 Sys.setenv("xgboost_datafolder"="D:/ff-dev/results")
 
 ## set variables ##
-laos= c("20N_100E","30N_100E")
 files=c("D:/ff-dev/results/20N_100E","D:/ff-dev/results/30N_100E")
 
 ## data quality check ##
@@ -15,9 +14,9 @@ quality_2 = ff_dqc(files[2])
 print(quality_2$summary)
 
 ## data preperation ##
-laos_train = ff_prep(country = "LAO", tiles=laos, end = c(2021,12), exc_features = c("pop2020","pop202","pop2030"), validation_sample = 0.1)
+laos_train = ff_prep(country = "LAO", end = c(2021,12), exc_features = c("pop2020","pop202","pop2030"), validation_sample = 0.1)
 
-laos_test =  ff_prep(country = "LAO", tiles=laos, start = c(2022,6), end = c(2023,5), exc_features = c("pop2020","pop202","pop2030"))
+laos_test =  ff_prep(country = "LAO", start = c(2022,6), end = c(2023,5), exc_features = c("pop2020","pop202","pop2030"))
 
 ## Run XGBoost ##
 
