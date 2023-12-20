593bd1b3db38b48b00c3c611066ab9f7e480340d
kingkong-WWF <jduijvenbode@wwf.nl>
2023-12-20 10:56:44 +0100

    add sin_month
    Sin months instead of relativedate

--- scripts_Zillah/Laos.R
+++ scripts_Zillah/Laos.R
@@ -9,13 +9,12 @@ files=c("D:/ff-dev/results/20N_100E","D:/ff-dev/results/30N_100E")
 
 ## data quality check ##
 quality_1 = ff_dqc(files[1])
-print(quality_1$summary)
+print(quality_1$byfeature)
 quality_2 = ff_dqc(files[2])
-print(quality_2$summary)
+print(quality_2$byfeature)
 
 ## data preperation ##
-laos_train = ff_prep(country = "LAO", end = c(2021,12), exc_features = c("pop2020","pop202","pop2030"), validation_sample = 0.1)
-
+laos_train = ff_prep(country = "LAO", end = c(2021,12), exc_features = c("pop2020","pop202","pop2030"))
 laos_test =  ff_prep(country = "LAO", start = c(2022,6), end = c(2023,5), exc_features = c("pop2020","pop202","pop2030"))
 
 ## Run XGBoost ##
