6efc3df7116d2c1fc0dd32664d8ac01bf598503c
jduijvenbode-WWF <149797028+jduijvenbode-WWF@users.noreply.github.com>
2023-12-27 09:53:08 +0100

    Update dowload_or_upload_s3.R

--- scripts_Zillah/dowload_or_upload_s3.R
+++ scripts_Zillah/dowload_or_upload_s3.R
@@ -4,17 +4,20 @@ tiles=list.files(file.path(inputdir), pattern ="_")
 
 Sys.setenv("AWS_DEFAULT_REGION"="eu-west-1")
 
-features= c("elevation.tif", "loss2001-2018.tif","loss2019.tif","loss2020.tif","loss2021.tif","loss2022.tif","slope.tif")
+features= c("pop2020.tif","pop2025.tif","pop2030.tif")
 
 for (tile in tiles){
   for (feature in features){
-    object_key <- paste0(tile, '/', feature)
-    del_key= paste0(tile, '/lastloss.tif')
+    #object_key <- paste0(tile, '/', feature)
+    #del_key= paste0(tile, '/lastloss.tif')
     local_path <- file.path(inputdir, tile, feature)
-    print(paste("Put", local_path, "to", object_key))
+    to_path <-paste0("D:/ff-dev/population_tiles/",tile,'/', feature)
+    #print(paste("Put", local_path, "to", object_key))
     #delete_object(object=del_key, bucket="wwf-ff-global")
     #put_object(file=local_path, object=object_key, bucket="wwf-ff-global")
-    save_object(object = object_key, bucket = "wwf-ff-global", file = local_path, quiet = FALSE, overwrite=TRUE)
+    #save_object(object = object_key, bucket = "wwf-ff-global", file = local_path, quiet = FALSE, overwrite=TRUE)
+    dir.create(paste0("D:/ff-dev/population_tiles/",tile))
+    file.rename(from=local_path, to=to_path )
   }
 }
 
