8925beeb0866e52d87c3be96e6f4682ee61914cb
jvduijvenbode <jonas@geotics.nl>
2023-12-29 15:15:46 +0100

    created a shorter version for data quality control and a minimum extent as input. also updated ff_prep to use that and to apply it on all rasters with different extents

--- package/ForestForesight/R/ff_dqcfile.R
+++ package/ForestForesight/R/ff_dqcfile.R
@@ -3,6 +3,7 @@
 #' requires a path to a raster as input and gives some quality parameters of that file
 #'
 #' @param raster path to raster.
+#' @param return_values Should the values of the rasters also be returned.
 #'
 #' @examples
 #' # Example usage:
@@ -11,8 +12,17 @@
 #' @export
 #'
 #'
- ff_dqc_file=function(raster){
+ ff_dqc_file=function(raster,return_values=T){
 loadras=rast(raster)
-return(list("npixel"=ncell(loadras),"xmin"=xmin(loadras),"xmax"=xmax(loadras),"ymin"=ymin(loadras),"ymax"=ymax(loadras),"resolution"=res(loadras)[1],
-            "crsname"=crs(loadras, describe=T)$name,"crscode"=crs(loadras, describe=T)$code,"mean"=as.numeric(round(global(loadras,"mean",na.rm=T),2)),"max"=as.numeric(round(global(loadras,"max",na.rm=T),2)),"hasNA"=(length(summary(loadras,warn=F))==7)))
+return(list("npixel"=ncell(loadras),
+            "xmin"=xmin(loadras),
+            "xmax"=xmax(loadras),
+            "ymin"=ymin(loadras),
+            "ymax"=ymax(loadras)
+            ,"resolution"=res(loadras)[1],
+            "crsname"=crs(loadras, describe=T)$name,
+            "crscode"=crs(loadras, describe=T)$code,
+            "mean"=if(return_values){as.numeric(round(global(loadras,"mean",na.rm=T),2))}else{NA},
+            "max"=if(return_values){as.numeric(round(global(loadras,"max",na.rm=T),2))}else{NA},
+            "hasNA"=if(return_values){(length(summary(loadras,warn=F))==7)}else{NA}))
 }
