82b386a8fed06ef57f5cadeac14a0dcb027844d5
jduijvenbode-WWF <149797028+jduijvenbode-WWF@users.noreply.github.com>
2024-01-08 19:04:47 +0100

    fixed path to preprocessing

--- preprocessing/latest_deforestation_loop_python.R
+++ preprocessing/latest_deforestation_loop_python.R
@@ -1,5 +1,5 @@
 
-source("C:/Users/EagleView/Documents/GitHub/ForestForesight/functions.R")
+source("C:/Users/EagleView/Documents/GitHub/ForestForesight/scripts_jonas/functions.R")
 files=list.files("D:/ff-dev/alerts/",full.names=T)
 files=sample(files,length(files))
 ffdates=paste(sort(rep(c(2021,2022,2023),12)),sprintf("%02d",seq(12)),"01",sep="-")[1:36]
@@ -10,7 +10,10 @@ for(file in files){
     print(ffdate)
     newfile=file.path(getwd(),createfilename(createrasname(file),date = ffdate,layer="layer"))
     diffdate=as.numeric(as.Date(ffdate)-as.Date("2015-01-01"))
-    commandtxt=paste("python","C:/Users/EagleView/Documents/GitHub/ForestForesight/IA-processing.py",file,newfile,diffdate)
+    commandtxt=paste("python","C:/Users/EagleView/Documents/GitHub/ForestForesight/preprocessing/IA-processing.py",file,newfile,diffdate)
+    if(ffdate>"2023-06-01"){
+      commandtxt=paste("python","C:/Users/EagleView/Documents/GitHub/ForestForesight/preprocessing/IA-processing.py",file,newfile,diffdate,0)
+    }
     print(commandtxt)
     system(commandtxt,intern=T)
   }
