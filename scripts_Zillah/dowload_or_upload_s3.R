4c0bc09a7026c7d54ce9406fa3ad23b30a0c3286
jduijvenbode-WWF <149797028+jduijvenbode-WWF@users.noreply.github.com>
2024-01-23 11:55:22 +0100

    Update dowload_or_upload_s3.R

--- scripts_Zillah/dowload_or_upload_s3.R
+++ scripts_Zillah/dowload_or_upload_s3.R
@@ -7,14 +7,15 @@ Sys.setenv("AWS_DEFAULT_REGION"="eu-west-1")
 features= c("_2001-01-01_landpercentage.tif")
 
 for (tile in tiles){
-  for (feature in features){
-    object_key <- paste0('preprocessed/',tile, '/', tile, feature)
-    #del_key= paste0(tile, '/', feature)
-    local_path <- file.path(inputdir, tile, paste0(tile,feature))
-    print(paste("put", object_key,"to", local_path))
+  files = list.files(file.path(inputdir,tile), pattern ="fire")
+  for (file in files){
+    object_key <- paste0('preprocessed/',tile, '/', file)
+    # del_key= paste0(tile, '/', feature)
+    local_path <- file.path(inputdir, tile, file)
+    print(paste("put", local_path,"to", object_key))
     #delete_object(object=del_key, bucket="wwf-ff-global")
     put_object(file=local_path, object=object_key, bucket="wwf-ff-global")
-    #save_object(object = object_key, bucket = "wwf-ff-global", file = local_path, quiet = FALSE, overwrite=TRUE)
+    # save_object(object = object_key, bucket = "wwf-ff-global", file = local_path, quiet = FALSE, overwrite=TRUE)
   }
 }
 
