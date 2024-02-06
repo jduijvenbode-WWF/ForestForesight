f7e0c5dd709bd578f56d3fe1238f35bfd5d4ae16
jduijvenbode-WWF <149797028+jduijvenbode-WWF@users.noreply.github.com>
2024-02-06 14:45:18 +0100

    Update run_on_created_models.R

--- scripts_jonas/run_on_created_models.R
+++ scripts_jonas/run_on_created_models.R
@@ -6,16 +6,24 @@ for(group in groups){
   tiles=gfw_tiles$tile_id[which(gfw_tiles$group==group)]
   model=readRDS(file.path("D:/ff-dev/results/models/",group,paste0(group,"_model.rds")))
   for(tile in tiles){
+    cat(paste(tile,"\n"))
     datr=daterange("2022-12-01","2024-01-01")
     datr=datr[c(seq(7),length(datr))]
     for(dr in datr){
+      cat(paste(dr,"\n"))
+      cat(paste("load data\n"))
       dat=ff_prep(datafolder = "D:/ff-dev/results/preprocessed/",tiles=tile,start = dr)
       if(dr=="2024-01-01"){
+        cat(paste("predict\n"))
       res=ff_predict(model = model,test_matrix = dat$data_matrix,templateraster = dat$groundtruthraster,certainty=T)
+      cat(paste("visualize\n"))
       vis=ff_visualize(res$predicted_raster,return_polygons = T,t_cutoff = 50)
+      cat(paste("contextualize\n"))
       con_vis=ff_contextualize(contextfolder = "D:/ff-dev/results/contextualization/",hotzones=list(vis),return_vector = T,outputfile = file.path("D:/ff-dev/results/hotzones",tile,paste0(tile,"_hotzones.json")))
       }else{
+        cat(paste("predict\n"))
         res=ff_predict(model = model,test_matrix = dat$data_matrix,templateraster = dat$groundtruthraster)
+        cat(paste("analyze\n"))
         ana=ff_analyze(res$predicted_raster,groundtruth = dat$groundtruthraster,csvfile = "D:/ff-dev/results/accuracy_analysis/2024-02-06_current_accuracy.csv",tile = tile,date = dr,return_polygons = F,append = T)
       }
     }
