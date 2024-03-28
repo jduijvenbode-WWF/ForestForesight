e0b204c19780464bcd8a7b93ac66e735d8728677
jvduijvenbode <jonas@geotics.nl>
2024-03-28 14:50:15 +0100

    new function to load all the features form the readme into a table

---
+++ package/ForestForesight/R/get_feature_metadata.R
@@ -0,0 +1,21 @@
+get_feature_metadata=function(){
+library(utils)
+
+# URL of the Markdown file
+url <- "https://raw.githubusercontent.com/jduijvenbode-WWF/ForestForesight/main/README.md"
+
+# Read the Markdown file
+markdown_text <- readLines(url)
+markdown_text <- gsub("#", "%23", markdown_text)
+# Extract the table from the Markdown text
+table_start <- grep("\\|feature", markdown_text)
+table_end <- grep("\\[\\^1\\]overview", markdown_text)-1
+table_text <- markdown_text[(table_start):(table_end)]
+
+# Read the table into a data frame
+df <- read.table(text = table_text, header = TRUE, sep = "|", fill = TRUE, stringsAsFactors = FALSE)
+df$source=gsub("%23","#",df$source)
+
+# Clean up column names
+colnames(df) <- trimws(gsub("\\|", "", colnames(df)))
+return(df)
