561f99f7a28dd19aa67ac815781812a3c59246aa
jvduijvenbode <jonas@geotics.nl>
2024-05-03 09:04:56 +0200

    Update baseline_v3.R

--- scripts_jonas/baseline_v3.R
+++ scripts_jonas/baseline_v3.R
@@ -1,7 +1,9 @@
-setwd("C:/data/colombia_tiles/")
-files=list.files(path="results",recursive=T,pattern="predictions_2")
-write.csv(files,"donefiles.csv")
-pols=vect("../global_1x1degree_polygons.json")
+setwd("C:/data/storage/preprocessed/")
+library(ForestForesight)
+files=list.files(recursive=T,pattern="groundtruth6m")
+
+data("degree_polygons")
+pols=vect(degree_polygons)
 calculate_scores=function(predfile,groundtruthfile,pols,adddate=T,fmaskfile=NULL){
   pred=rast(predfile)
   groundtruth=rast(groundtruthfile,win=ext(pred))
@@ -10,34 +12,34 @@ calculate_scores=function(predfile,groundtruthfile,pols,adddate=T,fmaskfile=NULL
   tile=basename(dirname(predfile))
   if(!is.null(fmaskfile)){
     cat("using forest mask")
-    fmask=rast(fmaskfile)
-    cross=2*groundtruth+pred*fmask
+    fmask=rast(fmaskfile)>0
+    cross=(2*groundtruth+pred)*fmask
   }else{cross=2*groundtruth+pred}
-  
+
   pols$FP=extract(cross==1,pols,fun="sum",na.rm=T,touches=F)[,2]
   pols$FN=extract(cross==2,pols,fun="sum",na.rm=T,touches=F)[,2]
   pols$TP=extract(cross==3,pols,fun="sum",na.rm=T,touches=F)[,2]
   pols$TN=extract(cross==0,pols,fun="sum",na.rm=T,touches=F)[,2]
   pols$date=date
   pols$tile=tile
+  pols=pols[-which(rowSums(as.data.frame(pols)[,c("FP","FN","TP")],na.rm=T)==0),]
   return(pols)
 }
-for(file in files){
-  print(file)
-  predfile=file.path("results",file)
-  fmaskfile=file.path("input",dirname(file),"forestmask2019.tif")
-  groundtruthfile=file.path("input",gsub("predictions","groundtruth",file))
-  calcpols=calculate_scores(predfile,groundtruthfile,pols)
-  if(file==files[1]){allpols=calcpols}else{allpols=rbind(allpols,calcpols)}
+for(x in 1:length(files)){
+  file=files[x]
+  cat(paste0(file," (",x," out of",length(files),")\n"))
+  blfile=gsub("groundtruth","input",gsub("groundtruth6m","lastsixmonths",file))
+  fmaskfile=file.path("input",basename(dirname(file)),paste0(basename(dirname(file)),"_2021-01-01_initialforestcover.tif"))
+
+  groundtruthfile=gsub("predictions","groundtruth",file)
+  calcpols=calculate_scores(predfile = blfile,groundtruthfile = groundtruthfile,pols = pols,fmaskfile = fmaskfile)
+  if(x==1){allpols=calcpols}else{allpols=rbind(allpols,calcpols)}
 }
 ap=as.data.frame(allpols)
-ap=ap[-which(rowSums(ap[,c("FP","FN","TP")],na.rm=T)==0),]
-ap2=read.csv("C:/data/results20231118.csv")
-ap2$X=NULL
-ap3=rbind(ap2,ap)
-write.csv(ap3,"C:/data/results20231118.csv")
+
+write.csv(ap,"C:/data/results20231118.csv")
 # calculate precision, recall, F1 and F0.5. These are not used in powerbi because the score also depends on the size of the polygon
-# so the scores below should be calculated on the highest order of scale you are presenting in the data 
+# so the scores below should be calculated on the highest order of scale you are presenting in the data
 # example: a polygon of 10x10 meters should not have the same impact on total F05 as a polygon of 10000x10000 meters with many more FN, FP, TP and TN
 
 
