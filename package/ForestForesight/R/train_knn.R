bc1c7d65f762f7b15dbbdc52113f3d51ef3cbdfc
Freek Gerrits Jans <freekgj@hotmail.com>
2024-05-10 16:32:39 +0200

    add crossvalidation and fix evaluation method

--- package/ForestForesight/R/train_knn.R
+++ package/ForestForesight/R/train_knn.R
@@ -4,6 +4,28 @@ library(ROCR)
 library(e1071)
 library(ggplot2)
 
+customPrSummary <- function(data, lev = NULL, model = NULL) {
+  # Assuming binary classification with levels
+  if (is.null(lev)) {
+    lev <- levels(data$obs)
+  }
+  # Setting the positive class
+  reference <- lev[2] # put the focus on deforested
+
+  # Calculate Precision and Recall
+  precision <- caret::posPredValue(data$pred, data$obs, positive = reference)
+  recall <- caret::sensitivity(data$pred, data$obs, positive = reference)
+
+  # Calculate F0.5 Score
+  F05 <- (1 + 0.5^2) * (precision * recall) / ((0.5^2 * precision) + recall)
+
+  # Return a named list including F0.5
+  out <- c(Precision = precision, Recall = recall, F0_5 = F05)
+  out[is.na(out)] <- 0  # Handling NaN or NA values
+  names(out) <- c("Precision", "Recall", "F0.5")
+  return(out)
+}
+
 train_model <- function(train_data, valid_data, save_model = FALSE) {
   max_cores <- parallel::detectCores(logical = TRUE)
   cl <- makeCluster(max_cores)
@@ -23,15 +45,15 @@ train_model <- function(train_data, valid_data, save_model = FALSE) {
   x_train <- predict(preProcValues, x_train)
   x_validation <- predict(preProcValues, x_validation)
 
-  ctrl <- trainControl(method = "cv", number = 5, allowParallel = TRUE,
+  ctrl <- trainControl(method = "repeatedcv", number = 5, repeats = 3, allowParallel = TRUE,
                        savePredictions = "final", classProbs = TRUE,
-                       summaryFunction = multiClassSummary)
-
+                       summaryFunction = customPrSummary, verboseIter = TRUE,
+                       )
 
   cat("Training model...\n")
   old_time <- Sys.time()
   model <- train(x = x_train, y = y_train, method = "knn",
-                 trControl = ctrl, tuneLength=10, metric='Sensitivity') #, preProc = manual_scale_function)
+                 trControl = ctrl, tuneGrid=expand.grid(k=c(7)), metric="F0.5")
 
   cat("Model trained\n")
   current_time <- Sys.time()
@@ -55,10 +77,10 @@ train_model <- function(train_data, valid_data, save_model = FALSE) {
   pred <- predict(model, newdata = x_validation)
 
     # Get values out of the confusion matrix
-  print(confusionMatrix(table(y_validation, pred)))
+  print(confusionMatrix(y_validation, pred, positive = "deforested"))
 
   stopCluster(cl)
   registerDoSEQ()
-
+  summary(model)
   return(model)
 }
