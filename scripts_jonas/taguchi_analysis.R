1c6a62960d48a0d33502eff08801b9ccdf851ee6
kingkong-WWF <103187046+ZillahCalle@users.noreply.github.com>
2024-05-17 16:46:04 +0200

    changes to scripts jonas

--- scripts_jonas/taguchi_analysis.R
+++ scripts_jonas/taguchi_analysis.R
@@ -7,64 +7,62 @@ abr ="LAO"
 params=read.csv("D:/ff-dev/Taguchi/TaguchiLaosFinal.csv")
 start=T
 #for(tile in tiles){
-  for(i in 1:nrow(params)){
-    if (params$forest_mask[i]==1)
-    {train_data = ff_prep(country=abr,start="2022-01-01", end = "2022-06-01", sample_size = params$datasample[i],fltr_features = "initialforestcover", fltr_condition = ">0")
-    }else{train_data = ff_prep(country=abr,start="2022-01-01", end = "2022-06-01", sample_size = params$datasample[i])
-    }
-    print(paste("Training data is loaded for expiriment", i, "and tile", tile))
+for(i in 1:nrow(params)){
+  if (params$forest_mask[i]==1)
+  {train_data = ff_prep(country=abr,start="2022-01-01", end = "2022-06-01",
+                        sample_size = params$datasample[i],
+                        fltr_features = "initialforestcover", fltr_condition = ">0", shrink="extract")
+  }else{train_data = ff_prep(country=abr,start="2022-01-01", end = "2022-06-01",
+                             sample_size = params$datasample[i], shrink="extract")
+  }
+  print(paste("Training data is loaded for expiriment", i))
 
-    ## train model ##
-    model_data = ff_train(train_matrix = train_data$data_matrix,validation_matrix=train_data$validation_matrix,
-                          verbose=F,
-                          nrounds = params$nrounds[i],eta = params$eta[i],
-                          max_depth = params$tree_depth[i],min_child_weight = params$min_child_weight[i],
-                          subsample = params$subsample[i],gamma = params$gamma[i])
-    print("Training finished")
+  ## train model ##
+  model_data = ff_train(train_matrix = train_data$data_matrix,validation_matrix=train_data$validation_matrix,
+                        verbose=F,
+                        nrounds = params$nrounds[i],eta = params$eta[i],
+                        max_depth = params$tree_depth[i],min_child_weight = params$min_child_weight[i],
+                        subsample = params$subsample[i],gamma = params$gamma[i])
+  print("Training finished")
 
-    ## 12 months Itteration  ##
-    ffdates <- daterange("2021-01-01","2023-05-01")
-    for(date in ffdates){
-      start_time = Sys.time()
-      if (params$forest_mask[i]==1){test_data =  ff_prep(country = abr, start = date,fltr_features = "initialforestcover", fltr_condition = ">0")
-      } else{test_data =  ff_prep(country = abr, start = date)}
+  ## 12 months Itteration  ##
+  ffdates <- daterange("2023-01-01","2023-05-01")
+  for(date in ffdates){
+    start_time = Sys.time()
+    if (params$forest_mask[i]==1){test_data =  ff_prep(country = abr, start = date,fltr_features = "initialforestcover", fltr_condition = ">0", shrink="extract")
+    } else{test_data =  ff_prep(country = abr, start = date, shrink="extract")}
 
-      predres=ff_predict(model_data,test_data$data_matrix,
-                         groundtruth=test_data$groundtruth)
-      iteration=c(date= date,params[i,],F05= predres$F0.5,precision=predres$precision,recall=predres$recall)
-      print(iteration)
-      if(start){start=F
-      results=iteration
-      }else{results=rbind(results,iteration)}
+    predres=ff_predict(model_data,test_data$data_matrix,
+                       groundtruth=test_data$groundtruth)
+    iteration=c(date= date,params[i,],F05= predres$F0.5,precision=predres$precision,recall=predres$recall)
+    print(iteration)
+    if(start){start=F
+    results=iteration
+    }else{results=rbind(results,iteration)}
 
-      print("writing results to csv")
-      write.csv(results,"D:/ff-dev/experiment_laos_param_final.csv")
-
-    }
+    print("writing results to csv")
+    write.csv(results,"D:/ff-dev/experiment_laos_param_final.csv")
 
   }
+
+}
 #  }
-
-
 
 ## analyse results ##
 
 results2= read.csv("D:ff-dev/experiment_laos_param_final.csv")
 df2=as.data.frame(results2)
 
-#tiles= c("00N_010E", "10N_080W","10N_110E", "10S_070W")
-
 #for (tile in tiles){
   result_list <- list()
 #  df= df2[df2$X.1==tile,]
-  for (i in colnames(df)[3:12]) {
-    result <- aggregate(df$X.3, by = list(df[[i]]), FUN = mean)
-    result_list[[i]] <- result
+  for (i in colnames(df2)[4:11]) {
+    result <- aggregate(df2$F05, by = list(df2[[i]]), FUN = mean)
+    select= result$Group.1[which.max(result$x)]
+    result_list[[i]] <- select
   }
   result_df <- do.call(rbind, result_list)
   print(result_df)
 #}
-
-# Combine the results into a single data frame
 
 
