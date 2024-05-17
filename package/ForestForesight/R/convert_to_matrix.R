81514541b2023260ab34e2bf48e50b21da32a74b
jduijvenbode-WWF <149797028+jduijvenbode-WWF@users.noreply.github.com>
2024-05-17 13:49:50 +0200

    add comments

--- package/ForestForesight/R/convert_to_matrix.R
+++ package/ForestForesight/R/convert_to_matrix.R
@@ -9,16 +9,29 @@
 #'
 #' @export
 convert_to_matrix <- function(files, daterange) {
+  # Function to select files by feature name
   select_by_featurename <- function(filenames, feature) {
     return(sort(filenames[which(substr(basename(filenames), 21, nchar(basename(filenames)) - 4) == feature)]))
   }
+
+  # Initialize an empty list to store matrices for each feature
   featurelist <- list()
+
+  # Iterate over each date in the date range
   for (i in daterange) {
+
+    # Select files for the current date
     selfiles <- select_files_date(i, files)
+
+    # Extract unique feature names from the selected files
     features <- unique(sapply(selfiles, function(x) substr(basename(x), 21, nchar(basename(x)) - 4)))
+
     curmat <- t(do.call(cbind, lapply(features, function(x) select_by_featurename(selfiles, x))))
+
+    # Set column names
     colnames(curmat) <- basename(dirname(curmat[1,]))
 
+    # Append the current matrix to the list of feature matrices
     featurelist <- append(featurelist, list(as.data.frame(curmat)))
   }
   return(featurelist)
