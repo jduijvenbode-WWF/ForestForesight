160ff6a97c4fc77eefdb3201ff5716aa06ed8777
Maas van Apeldoorn <m.a.a.s.v.apeldoorn@tue.nl>
2024-10-22 15:43:36 +0200

    now actually added it to the repo

---
+++ R/run_checks.R
@@ -0,0 +1,56 @@
+run_all_checks <- function() {
+  # Lint the package
+  cat("Running lint...\n")
+  lintr::lint_package()
+
+  # Style the code
+  cat("Styling code...\n")
+  styler::style_pkg()
+
+  # Run tests
+  cat("Running tests...\n")
+  devtools::test()
+
+  # Optionally, check package
+  cat("Running devtools::check()...\n")
+  devtools::check()
+}
+
+run_lintr_and_styler_for_file <- function(file_string) {
+  cat("Styling file ", file_string, "\n")
+  styler::style_file(file_string)
+  cat("Linting file ", file_string, "\n")
+  run_lintr_for_file(file_string)
+}
+
+run_test_active_file <- function() {
+  devtools::test_active_file()
+  cat("test coverage: ", devtools::test_coverage_active_file())
+}
+
+run_lint_style_package <- function() {
+  # Style the code
+  cat("Styling code...\n")
+  styler::style_pkg()
+  # Lint the package
+  cat("Running lint...\n")
+  lintr::lint_package()
+}
+
+run_tests <- function() {
+  devtools::test()
+  devtools::test_coverage()
+}
+
+run_CRAN_test <- function() {
+  devtools::check()
+}
+
+run_lintr_for_file <- function(file_string) {
+  lint_result <- lintr::lint(file_string)
+  cat("Style violations: \n")
+  print(lint_result)
+  num_lints <- length(lint_result)
+  cat("Number of style violations: ", num_lints, "\n")
+}
+
