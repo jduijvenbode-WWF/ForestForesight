47b075eeeb27a59b0f224e737cb11eb7df9c66ed
Hasrul <hasrulwho@gmail.com>
2024-10-28 13:45:48 +0100

    adjust comment/text in testthat/helper_config

--- tests/testthat/helper_config.R
+++ tests/testthat/helper_config.R
@@ -1,9 +1,9 @@
-# setup_config.R
+# helper_config.R
 
 config_load()
 
 # Print a message to indicate the setup file has been executed
-print("setup_config.R has been executed")
+print("helper_config.R has been executed")
 
 cat("TEST_FF_PREP_COUNTRY is ")
 cat(Sys.getenv("TEST_FF_PREP_COUNTRY"))
