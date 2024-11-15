83865242fbe60b37f9296aef1dbfdb489f770f60
maasva <maasvapeldoorn@gmail.com>
2024-11-15 14:18:37 +0100

    Fixed test case to deal with it not returning any values.

--- tests/testthat/test-check_pre_conditions.R
+++ tests/testthat/test-check_pre_conditions.R
@@ -4,49 +4,44 @@ library(testthat)
 dates <- Sys.getenv("TEST_FF_PREP_QC_DATE")
 country <- Sys.getenv("TEST_FF_PREP_COUNTRY")
 datafolder <- Sys.getenv("TEST_DATA_FOLDER")
-# cat("From test-QC: the country is ")
-# cat(country)
 shape <- NULL
 tiles <- NULL
 shrink <- "none"
 
 test_that("check_pre_conditions works as expected", {
-  # Test valid input, expecting to return a list with datafolder and shrink
-  result <- check_pre_conditions(dates, country, shape, tiles, datafolder, shrink)
-  expect_equal(result$datafolder, datafolder)
-  expect_equal(result$shrink, shrink)
-
-  # Test missing country and shape, expecting shrink to be set to 'none'
-  result <- check_pre_conditions(dates, country, NULL, tiles, datafolder, shrink)
-  expect_equal(result$shrink, "none")
+  # Test valid input, expecting to not throw an error
+  expect_no_error(
+    check_pre_conditions(dates, country, shape, tiles, shrink),
+    message = "valid input should not throw an error"
+  )
 
   # Test missing dates with NA
   expect_error(
-    check_pre_conditions(NA, country, shape, tiles, datafolder, shrink),
+    check_pre_conditions(NA, country, shape, tiles, shrink),
     "No dates were given"
   )
 
   # Test missing dates with NULL
   expect_error(
-    check_pre_conditions(NULL, country, shape, tiles, datafolder, shrink),
+    check_pre_conditions(NULL, country, shape, tiles, shrink),
     "No dates were given"
   )
 
   # Test missing dates with an empty string
   expect_error(
-    check_pre_conditions("", country, shape, tiles, datafolder, shrink),
+    check_pre_conditions("", country, shape, tiles, shrink),
     "No dates were given"
   )
 
   # Test missing tiles, country, and shape
   expect_error(
-    check_pre_conditions(dates, NULL, NULL, NULL, datafolder, shrink),
+    check_pre_conditions(dates, NULL, NULL, NULL, shrink),
     "Unknown what to process since no tiles, country, or shape were given"
   )
 
   # Test shape is not SpatVector
   expect_error(
-    check_pre_conditions(dates, country, "invalid_shape", tiles, datafolder, shrink),
+    check_pre_conditions(dates, country, "invalid_shape", tiles, shrink),
     "Shape should be of class SpatVector"
   )
 })
