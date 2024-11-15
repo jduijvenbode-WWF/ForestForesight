68559a2e651b6e262735f4811a3396ecbc67a998
jvduijvenbode <149797028+jduijvenbode-WWF@users.noreply.github.com>
2024-11-15 09:58:15 +0100

    Update ff_structurecheck.R

--- R/ff_structurecheck.R
+++ R/ff_structurecheck.R
@@ -8,6 +8,9 @@
 #' @param folder_path Character string. Path to the main ForestForesight folder.
 #' @param check_date Character string. Date to check for in the format "YYYY-MM-DD".
 #'        If NULL, uses the first of the current month.
+#' @param error_on_issue Logical. Whether the function should return an error if critical
+#' issues are not handled correctly
+#' @param silent_on_pass Logical. Whether correct checks should not be output
 #'
 #' @return Invisible NULL. The function is called for its side effects (printing).
 #'
@@ -15,7 +18,7 @@
 #' @importFrom lubridate floor_date
 #'
 #' @export
-ff_structurecheck <- function(shape, folder_path, check_date = NULL) {
+ff_structurecheck <- function(shape, folder_path, check_date = NULL, error_on_issue = FALSE, silent_on_pass = FALSE) {
   # Get info from shape
   info <- getinfo(shape, verbose = FALSE)
 
@@ -29,68 +32,73 @@ ff_structurecheck <- function(shape, folder_path, check_date = NULL) {
   main_folders <- c("preprocessed", "models", "predictions")
   for (folder in main_folders) {
     if (!dir.exists(file.path(folder_path, folder))) {
-      ff_cat(paste("No", folder, "folder present\n"), color = "red")
+      print_result(paste("No", folder, "folder present\n"), color = "red")
     } else {
-      ff_cat(paste(folder, "folder present\n"), color = "green")
+      print_result(paste(folder, "folder present"), color = "green", verbose = !silent_on_pass)
     }
+  }
+  if (any(!dir.exists(main_folders)) && error_on_issue){
+    stop("some main folders are missing.
+         Fix issues as printed above and try again")
   }
 
   # Check preprocessed subfolders
-  cat("\nChecking preprocessed folder\n")
+  ff_cat("\nChecking preprocessed folder")
   prep_subfolders <- c("input", "groundtruth")
   for (subfolder in prep_subfolders) {
     folder <- file.path(folder_path, "preprocessed", subfolder)
     if (!dir.exists(folder)) {
-      ff_cat(paste("No", subfolder, "subfolder in preprocessed\n"), color = "red")
+      print_result(paste("No", subfolder, "subfolder in preprocessed"), color = "red",error_on_issue = error_on_issue)
+
     } else {
-      ff_cat(paste(subfolder, "subfolder present in preprocessed\n"), color = "green")
+      print_result(subfolder, "subfolder present in preprocessed", color = "green", verbose = !silent_on_pass)
       # Check tile subfolders
       for (tile in info$tile_ids) {
         if (!dir.exists(file.path(folder, tile))) {
-          ff_cat(paste("No subfolder for tile", tile, "in", subfolder, "\n"), color = "red")
+          print_result("No subfolder for tile", tile, "in", subfolder, color = "red",error_on_issue = error_on_issue)
         } else {
-          ff_cat(paste("Subfolder for tile", tile, "present in", subfolder, "\n"), color = "green")
+          print_result("Subfolder for tile", tile, "present in", subfolder, color = "green", verbose = !silent_on_pass)
           # Check for correct file naming and date
           files <- list.files(file.path(folder, tile), pattern = "\\.tif$")
           if (length(files) == 0) {
-            ff_cat(paste("No tif files in", subfolder, "for tile", tile, "\n"), color = "red")
+            print_result("No tif files in", subfolder, "for tile", tile, color = "red",error_on_issue = error_on_issue)
           } else {
             correct_name_pattern <- paste0("^", tile, "_\\d{4}-\\d{2}-01_[^_]+\\.tif$")
             incorrect_files <- files[!grepl(correct_name_pattern, files)]
             if (length(incorrect_files) > 0) {
-              ff_cat(paste("Incorrect file naming in", subfolder, "for tile", tile, ":\n"), color = "red")
+              print_result("Incorrect file naming in", subfolder, "for tile", tile, ":", color = "red",error_on_issue = FALSE)
               for (file in incorrect_files) {
-                ff_cat(paste("  ", basename(file), "\n"), color = "red")
+                print_result("  ", basename(file), color = "red")
               }
+              if (error_on_issue) {stop("please fix issues above and try again")}
             } else {
-              ff_cat(paste("Correct file naming in", subfolder, "for tile", tile, "\n"), color = "green")
+              print_result(paste("Correct file naming in", subfolder, "for tile", tile, "\n"), color = "green", silent_on_pass = silent_on_pass)
             }
             has_check_date <- any(grepl(paste0("^", tile, "_", check_date, "_lastsixmonths.tif$"), files))
             if (subfolder != "groundtruth") {
               if (!has_check_date) {
-                ff_cat(paste(
+                print_result(
                   "No EWS features for check date", check_date, "in", subfolder,
-                  "for tile", tile, "\n"
-                ), color = "yellow")
+                  "for tile", tile,
+                color = "yellow")
               } else {
-                ff_cat(paste(
+                print_result(
                   "EWS features for check date", check_date, "present in", subfolder,
-                  "for tile", tile, "\n"
-                ), color = "green")
+                  "for tile", tile, color = "green", silent_on_pass = silent_on_pass)
               }
             }
             if (subfolder == "groundtruth") {
               has_groundtruth6m <- any(grepl(paste0("^", tile, "_", check_date, "_groundtruth6m\\.tif$"), files))
               if (!has_groundtruth6m) {
-                ff_cat(paste(
+                print_result(paste(
                   "No groundtruth6m file for check date", check_date,
                   "in groundtruth for tile", tile, "\n"
                 ), color = "yellow")
               } else {
-                ff_cat(paste(
+                print_result(
                   "Groundtruth6m file present for check date", check_date,
                   "in groundtruth for tile", tile, "\n"
-                ), color = "green")
+                , color = "green",silent_on_pass = silent_on_pass)
               }
             }
           }
@@ -105,20 +113,20 @@ ff_structurecheck <- function(shape, folder_path, check_date = NULL) {
   for (group in info$country_groups) {
     group_folder <- file.path(folder_path, "models", group)
     if (!dir.exists(group_folder)) {
-      ff_cat(paste("No subfolder for group", group, "in models\n"), color = "red")
+      print_result("No subfolder for group", group, "in models", color = "red",error_on_issue = error_on_issue)
     } else {
-      ff_cat(paste("Subfolder for group", group, "present in models\n"), color = "green")
+      print_result("Subfolder for group", group, "present in models", color = "green",silent_on_pass = silent_on_pass)
       model_file <- file.path(group_folder, paste0(group, ".model"))
       rda_file <- file.path(group_folder, paste0(group, ".rda"))
       if (!file.exists(model_file)) {
-        ff_cat(paste("No .model file for group", group, "\n"), color = "yellow")
+        print_result(paste("No .model file for group", group, "\n"), color = "yellow")
       } else {
-        ff_cat(paste(".model file present for group", group, "\n"), color = "green")
+        print_result(paste(".model file present for group", group, "\n"), color = "green",silent_on_pass = silent_on_pass)
       }
       if (!file.exists(rda_file)) {
-        ff_cat(paste("No .rda file for group", group, "\n"), color = "red")
+        print_result(paste("No .rda file for group", group, "\n"), color = "red",error_on_issue = error_on_issue)
       } else {
-        ff_cat(paste(".rda file present for group", group, "\n"), color = "green")
+        print_result(paste(".rda file present for group", group, "\n"), color = "green",silent_on_pass = silent_on_pass)
       }
     }
   }
@@ -130,22 +138,32 @@ ff_structurecheck <- function(shape, folder_path, check_date = NULL) {
     iso3 <- countries$iso3[countries$name == cname]
     iso3_folder <- file.path(folder_path, "predictions", iso3)
     if (!dir.exists(iso3_folder)) {
-      ff_cat(paste("No subfolder for ISO3 code", iso3, "in predictions\n"), color = "red")
+      print_result(paste("No subfolder for ISO3 code", iso3, "in predictions\n"), color = "red",error_on_issue = error_on_issue)
     } else {
-      ff_cat(paste("Subfolder for ISO3 code", iso3, "present in predictions\n"), color = "green")
+      print_result(paste("Subfolder for ISO3 code", iso3, "present in predictions\n"), color = "green",silent_on_pass = silent_on_pass)
       files <- list.files(iso3_folder, pattern = "\\.tif$")
       if (length(files) == 0) {
-        ff_cat(paste("No tif files in predictions for ISO3 code", iso3, "\n"), color = "yellow")
+        print_result(paste("No tif files in predictions for ISO3 code", iso3, "\n"), color = "yellow")
       } else {
         correct_name <- all(grepl(paste0("^", iso3, "_\\d{4}-\\d{2}-01\\.tif$"), files))
         if (!correct_name) {
-          ff_cat(paste("Incorrect file naming in predictions for ISO3 code", iso3, "\n"), color = "red")
+          print_result("Incorrect file naming in predictions for ISO3 code", iso3, color = "red",error_on_issue = error_on_issue)
         } else {
-          ff_cat(paste("Correct file naming in predictions for ISO3 code", iso3, "\n"), color = "green")
+          print_result("Correct file naming in predictions for ISO3 code", iso3, color = "green",silent_on_pass = silent_on_pass)
         }
       }
     }
   }
 
   invisible(NULL)
+}
+
+print_result = function(...,color,silent_on_pass = FALSE, error_on_issue = FALSE){
+  statement <- paste(..., sep = " ")
+  if(silent_on_pass && color == "green"){ invisible(NULL)}else{
+    if(color == "red" && error_on_issue){
+      stop(statement)}else{
+        ff_cat(statement,color = color)
+      }
+  }
 }
