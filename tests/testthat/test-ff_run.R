17655e6f639ac6bc10009af5a1d9c5b250b8c4ad
jvduijvenbode <149797028+jduijvenbode-WWF@users.noreply.github.com>
2024-11-27 16:34:52 +0100

    made ff_run unit tests non verbose for readibility

--- tests/testthat/test-ff_run.R
+++ tests/testthat/test-ff_run.R
@@ -3,8 +3,6 @@ test_that("check that ff_run can handle ", {
   tiffile <- file.path(test_dir, "result.tif")
   tiffile2 <- file.path(test_dir, "result2.tif")
   datadir <- file.path(getwd(), "../test_data/")
-  print(datadir)
-  print(dir.exists(datadir))
   modelfile <- file.path(test_dir, "model.model")
   testthat::expect_no_error(
     result1 <- ff_run(
@@ -13,16 +11,16 @@ test_that("check that ff_run can handle ", {
       ff_folder = datadir,
       ff_prep_params = list(inc_features = c("initialforestcover", "timesinceloss")),
       save_path_predictions = tiffile,
-      save_path = modelfile
+      save_path = modelfile, verbose = F
     )
   )
 
   testthat::expect_no_error(
-    result2 <- ff_run(country = "BRN", prediction_dates = "2023-03-01", ff_folder = datadir, save_path_predictions = tiffile2, trained_model = modelfile)
+    result2 <- ff_run(country = "BRN", prediction_dates = "2023-03-01", ff_folder = datadir, save_path_predictions = tiffile2, trained_model = modelfile, verbose = F)
   )
   testthat::expect_equal(global(rast(tiffile), "sum"), global(rast(tiffile2), "sum"))
   countries <- terra::vect(get(data("countries")))
   countries <- countries[countries$iso3 == "BRN"]
-  testthat::expect_no_error(result3 <- ff_run(shape = countries, prediction_dates = "2023-03-01", ff_folder = datadir, save_path_predictions = tiffile, trained_model = modelfile))
-  testthat::expect_no_error(result4 <- ff_run(shape = countries, train_dates = "2023-02-01", ff_folder = datadir, save_path = modelfile))
+  testthat::expect_no_error(result3 <- ff_run(shape = countries, prediction_dates = "2023-03-01", ff_folder = datadir, save_path_predictions = tiffile, trained_model = modelfile, verbose = F))
+  testthat::expect_no_error(result4 <- ff_run(shape = countries, train_dates = "2023-02-01", ff_folder = datadir, save_path = modelfile, verbose = F))
 })
