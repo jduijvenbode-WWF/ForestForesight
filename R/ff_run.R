7cf2021e0a8438ab7e19b74995568b39eb89568b
jvduijvenbode <149797028+jduijvenbode-WWF@users.noreply.github.com>
2024-11-27 21:36:33 +0100

    removed the convert_to_matrix, fitler_by_feature and merge_lists functions
     because they are only used by one main function. I added the noRd tag and added them to the bottom of the main function.

--- R/ff_run.R
+++ R/ff_run.R
@@ -378,3 +378,32 @@ ff_run <- function(shape = NULL, country = NULL, prediction_dates = NULL,
   }
   return(allras)
 }
+
+
+#' Merge Two Named Lists with User Preferences Overwriting Defaults
+#'
+#' This function merges two named lists, allowing elements from the user-specified list
+#' to overwrite those in the default list if they have the same names. If there are no
+#' overlapping keys, the function simply combines the lists.
+#'
+#' @param default A named list containing the default values.
+#' @param user A named list containing the user-specified values that may overwrite the default values.
+#'
+#' @return A named list with combined elements from both input lists. User-specified elements
+#' overwrite default elements with the same names.
+#'
+#' @examples
+#' default_list <- list(a = 1, b = 2, c = 3)
+#' user_list <- list(b = 20, d = 4)
+#' merged_list <- merge_lists(default_list, user_list)
+#' print(merged_list) # Should print: $a [1], $c [3], $b [20], $d [4]
+#'
+#' @noRd
+merge_lists <- function(default, user) {
+  indices <- -which(names(default) %in% names(user))
+  if (length(indices) != 0) {
+    c(default[indices], user)
+  } else {
+    c(default, user)
+  }
+}
