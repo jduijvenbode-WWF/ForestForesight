66abff35774ec9994b1350d594832e8ea1dd226f
hasruleng <hasrulwho@gmail.com>
2024-11-29 00:53:42 +0100

    test-ff_run datadir using value from  env.yml

--- tests/testthat/helper_config.R
+++ tests/testthat/helper_config.R
@@ -7,37 +7,10 @@ if (file.exists(config_file_path)) {
   config_file_path <- file.path(getwd(), "../env.yml")
   print(config_file_path)
 
-  parent_dir <- file.path(getwd(), "../../../..")
-  print(paste("Listing files in ../../../.. from rcmdcheck testthat:", parent_dir))
-  # List all files and directories in the parent directory
-  print(list.files(parent_dir, full.names = TRUE))
-
-  parent_dir <- file.path(getwd(), "../../..")
-  print(paste("Listing files in ../../.. from rcmdcheck testthat:", parent_dir))
-  # List all files and directories in the parent directory
-  print(list.files(parent_dir, full.names = TRUE))
-
-  parent_dir <- file.path(getwd(), "../..")
-  print(paste("Listing files in ../.. from rcmdcheck testthat:", parent_dir))
-  # List all files and directories in the parent directory
-  print(list.files(parent_dir, full.names = TRUE))
-
-  parent_dir <- file.path(getwd(), "..")
-  print(paste("Listing files in .. from rcmdcheck testthat:", parent_dir))
-  # List all files and directories in the parent directory
-  print(list.files(parent_dir, full.names = TRUE))
-
-
-  parent_dir <- file.path(getwd(), ".")
-  print(paste("Listing files in . from rcmdcheck testthat:", parent_dir))
-  # List all files and directories in the parent directory
-  print(list.files(parent_dir, full.names = TRUE))
-
-
   if (file.exists(config_file_path)) {
-    print("------../../env.yml exists!")
+    print("------../env.yml exists!")
   } else {
-    message("How come ../../env.yml doesn't exist!")
+    message("How come ../env.yml doesn't exist!")
   }
 }
 config_load(config_file_path)
