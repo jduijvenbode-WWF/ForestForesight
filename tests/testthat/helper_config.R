7ad7553ae10a54fdc3000abe59188b1e2c0490c9
hasruleng <hasrulwho@gmail.com>
2024-11-28 23:49:51 +0100

    I can see the detail now. more debugging

--- tests/testthat/helper_config.R
+++ tests/testthat/helper_config.R
@@ -3,9 +3,9 @@ config_file_path <- here::here("env.yml")
 if (file.exists(config_file_path)) {
   print("wd is in the root => the normal test")
 } else {
-  print("============probably in rcmdcheck")
-  message("======================probably in rcmdcheck")
+  print("============probably in rcmdcheck. printing new config_file_path")
   config_file_path <- file.path(getwd(), "../../env.yml")
+  print(config_file_path)
   if (file.exists(config_file_path)) {
     print("------../../env.yml exists!")
     message("-------../../env.yml exists!")
