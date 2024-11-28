b00b607a230a98f3b727debabafef2119d5c8545
hasruleng <hasrulwho@gmail.com>
2024-11-29 00:12:56 +0100

    from rcmdcheck printing list of files in root dir

--- tests/testthat/helper_config.R
+++ tests/testthat/helper_config.R
@@ -6,9 +6,17 @@ if (file.exists(config_file_path)) {
   print("============probably in rcmdcheck. printing new config_file_path")
   config_file_path <- file.path(getwd(), "../../env.yml")
   print(config_file_path)
+  
+  parent_dir <- file.path(getwd(), "../../")
+  print(paste("Listing files in directory:", parent_dir))
+  
+  # List all files and directories in the parent directory
+  print(list.files(parent_dir, full.names = TRUE))
+
   if (file.exists(config_file_path)) {
     print("------../../env.yml exists!")
-    message("-------../../env.yml exists!")
+  } else {
+    message("How come ../../env.yml doesn't exist!")
   }
 }
 config_load(config_file_path)
