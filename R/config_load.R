e2674e52d4c319a03a65397722664efeefe9b2b1
hasruleng <hasrulwho@gmail.com>
2024-11-29 03:05:05 +0100

    optionally load user config.yml

--- R/config_load.R
+++ R/config_load.R
@@ -10,19 +10,21 @@ config_load <- function(config_file_path = "") {
   }
   if (file.exists(config_file)) {
     load_variables(config_file)
+    
+    # user_config_file is used by users to replace or supplement default configuration
+    user_config_file <- here::here("config.yml")
+    if (file.exists(user_config_file)) { # optionally load the user config_file
+      load_variables(user_config_file)
+    }
   } else {
     warning("The config file does not exist. Please check the file path.")
   }
 }
 
 load_variables <- function(config_file_path) {
-  # Load the YAML file
-  print("config file message: ")
-  config_file_message <- paste("=== loading into environment variables: ", config_file_path)
-
+  # Load the YML file
   config <- yaml::yaml.load_file(config_file_path)
   config_file_message <- paste(config_file_message, "\n", config)
-  print(config_file_message)
   # Set environment variables
   set_env_vars <- function(config_list, prefix = "") {
     for (name in names(config_list)) {
