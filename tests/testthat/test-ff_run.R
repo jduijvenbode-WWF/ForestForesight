691e7e84b0022faef6425bda1619e6f1b537c307
jvduijvenbode <149797028+jduijvenbode-WWF@users.noreply.github.com>
2024-12-02 20:12:34 +0100

    added a test to see if ff_run works if it has multiple polygons and it does

--- tests/testthat/test-ff_run.R
+++ tests/testthat/test-ff_run.R
@@ -23,4 +23,7 @@ test_that("check that ff_run can handle ", {
   countries <- countries[countries$iso3 == "BRN"]
   testthat::expect_no_error(result3 <- ff_run(shape = countries, prediction_dates = "2023-03-01", ff_folder = datadir, save_path_predictions = tiffile, trained_model = modelfile, verbose = F))
   testthat::expect_no_error(result4 <- ff_run(shape = countries, train_dates = "2023-02-01", ff_folder = datadir, save_path = modelfile, verbose = F))
+  countries <- disagg(countries)
+
+  testthat::expect_no_error(result4 <- ff_run(shape = countries, train_dates = "2023-02-01",prediction_dates = "2023-03-01", ff_folder = datadir, save_path = modelfile, verbose = F))
 })
