aaa387f4e3a9bc8dde3ec1511c216127b9d2cb8c
hasruleng <hasrulwho@gmail.com>
2024-12-04 00:06:54 +0100

    remove warning messages when loading FF (onLoad)

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,4 +1,6 @@
 .onLoad <- function(libname, pkgname) {
   print(".onLoad is called")
+  # remove Warning: replacing previous import 'smoothr::densify' by 'terra::densify'
+  conflicted::conflicts_prefer(terra::densify) 
   config_load() # Call the config_load function when the package is loaded
 }
