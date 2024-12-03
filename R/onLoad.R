75ce4beba1f4a5e68a8787b09c8bcc1f177b8863
hasruleng <hasrulwho@gmail.com>
2024-12-04 00:44:15 +0100

    remove warning messages when loading FF (onLoad) 2

--- R/onLoad.R
+++ R/onLoad.R
@@ -3,6 +3,9 @@
   # remove Warning: replacing previous import 'smoothr::densify' by 'terra::densify'
   if (requireNamespace("conflicted", quietly = TRUE)) {
     conflicted::conflicts_prefer(terra::densify)
+    suppressWarnings({
+      conflicted::conflict_scout()  # This can help identify other potential conflicts
+    })
   } else {
     message("Package 'conflicted' is not installed. Preferred conflict for 'terra::densify' not set.")
   }
