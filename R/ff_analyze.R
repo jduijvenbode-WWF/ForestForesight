8b71979e83839a308dbeab999f2d84dea441cb4a
jvduijvenbode <149797028+jduijvenbode-WWF@users.noreply.github.com>
2024-12-19 16:23:56 +0100

    Update ff_analyze.R

--- R/ff_analyze.R
+++ R/ff_analyze.R
@@ -147,11 +147,10 @@ calculate_scores_crosstable <- function(crosstable_raster, polygons, verbose) {
       (sum(polygons$TP, na.rm = TRUE) + sum(polygons$FP, na.rm = TRUE))
     recall <- sum(polygons$TP, na.rm = TRUE) /
       (sum(polygons$TP, na.rm = TRUE) + sum(polygons$FN, na.rm = TRUE))
-    ff_cat("F0.5 score:", round(1.25 * precision * recall / (0.25 * precision + recall),2),
-           "\nprecision:", round(precision,2),
-           "\nrecall:", round(recall,2),
-           verbose = verbose
-    )
+    ff_cat("F0.5 score:", round(1.25 * precision * recall / (0.25 * precision + recall),2),verbose = verbose)
+    ff_cat("precision:", round(precision,2),verbose = verbose)
+    ff_cat("recall:", round(recall,2),verbose = verbose)
+
   }
   return(polygons)
 }
